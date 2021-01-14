126215b833f31958d499e23c9095049ae93603c8
barnij <barnij@windowslive.com>
2021-01-14 01:17:37 +0100

    delete all precompiled bootstrap files, move static files to the public directory, move content from index.html to ejs file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const http = require('http');
+var path = require('path')
+var __dirname = path.resolve()
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
