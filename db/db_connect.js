09964e72d9e6691ca98e1c643aa9a381767a84cc
Andrzej Tkaczyk <309181@uwr.edu.pl>
2021-01-16 20:34:24 +0100

    added some functions to db connection

--- db/db_connect.js
+++ db/db_connect.js
@@ -1,5 +1,15 @@
 const { Pool } = require('pg');
 
+function send (query, pool) {
+  (async () => {
+    try{
+      let client = await pool.connect();
+      let res = await client.query(query);
+    } catch {
+      console.error(err);
+    }
+  })();
+}
 function connect() {
   let pool = new Pool({
     user: 'postgres',
@@ -11,19 +21,44 @@ function connect() {
   pool.on('error', (err, client) => {
     console.error('Error:', err);
   });
-  return pool
+  return pool;
 }
 function disconnect(pool) {
-  pool.end()
+  pool.end();
 }
-function add_user(username, password, isadmin, pool) {
+function check_user_existence(pool, username, password) {
+  let query = `
+  SELECT id FROM account
+  WHERE username = ${username}
+  AND password = ${password}
+  `;
+  (async () => {
+    try{
+      let client = await pool.connect();
+      let res = await client.query(query);
+      tmp = "";
+      for (let row in res.rows) {
+        tmp += row;
+      }
+      let user = JSON.parse(tmp)
+      return user.id;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  })();
+}
+function add_user(pool, username, password, isadmin) {
   if (isadmin) {
-    isadmin = 't'
+    isadmin = 't';
   }
   else {
-    isadmin = 'f'
+    isadmin = 'f';
   }
-  //TODO check if already exists
+  if(check_user_existence(pool, username, password)) {
+    console.error('adding existing user attempt');
+    return false;
+  }
   let query = `
   INSERT INTO account (username, password, isadmin, last_login)
   VALUES (${username}, ${password}, ${isadmin}, NOW())
@@ -39,7 +74,15 @@ function add_user(username, password, isadmin, pool) {
     }
   })();
 }
-function login_user(username, password, pool) {
+function update_last_login(pool, userid) {
+  let query = `
+  UPDATE account
+  SET last_login = NOW()
+  WHERE id = ${userid}
+  `;
+  send(query, pool);
+}
+function login_user(pool, username, password) {
   let query = `
   SELECT id FROM account
   WHERE username = ${username}
@@ -55,7 +98,7 @@ function login_user(username, password, pool) {
       }
       let user = JSON.parse(tmp)
       if (user.id) {
-        //TODO update last login
+        update_last_login(user.id, pool);
         return user.id;
       } else {
         return false;
@@ -66,5 +109,92 @@ function login_user(username, password, pool) {
     }
   })();
 }
-
-return {connect, disconnect, add_user, login_user};
+function set_admin(pool, userid, isadmin) {
+  if (isadmin) {
+    isadmin = 't';
+  }
+  else {
+    isadmin = 'f';
+  }
+  let query = `
+  UPDATE account
+  SET isadmin = ${isadmin}
+  WHERE id = ${userid}
+  `;
+  send(query, pool);
+}
+function add_category(pool, description) {
+  let query = `
+  INSERT INTO category (description)
+  VALUES (${description})
+  `;
+  send(query, pool);
+}
+function add_size(pool, description) {
+  let query = `
+  INSERT INTO size (description)
+  VALUES (${description})
+  `;
+  send(query, pool);
+}
+function add_colour(pool, description) {
+  let query = `
+  INSERT INTO colour (description)
+  VALUES (${description})
+  `;
+  send(query, pool);
+}
+function add_product(pool, name, category, size, colour, description, price, status, amount=0) {
+  if (status) {
+    status = 't';
+  } else {
+    status = 'f';
+  }
+  let query = `
+  INSERT INTO product (price, name, size, colour, amount, status, description, category)
+  VALUES (${price}, ${name}, ${size}, ${colour}, ${amount}, ${status}, ${description}, ${category})
+  `;
+  send(query, pool);
+}
+function set_product_status (pool, prodid, status) {
+  if (status) {
+    status = 't';
+  } else {
+    status = 'f';
+  }
+  let query = `
+  UPDATE product
+  SET status = ${status}
+  WHERE id = ${prodid}
+  `;
+  send(query, pool);
+}
+function add_product_amount (pool, prodid, amount) {
+  let query = `
+  UPDATE product
+  SET amount = amount+${amount}
+  WHERE id = ${prodid}
+  `;
+  send(query, pool);
+}
+function add_purchase_status(pool, description) {
+  let query = `
+  INSERT INTO purchase_status (description)
+  VALUES (${description})
+  `;
+  send(query, pool);
+}
+//TODO getters
+//TODO purchase management
+return {connect,
+        disconnect,
+        add_user,
+        login_user,
+        set_admin, 
+        add_category, 
+        add_size, 
+        add_colour, 
+        add_product, 
+        add_purchase_status, 
+        set_product_status,
+        add_product_amount};
