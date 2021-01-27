fb10633d6bd86672ca3c3a7740df227af2a5890c
barnij <barnij@windowslive.com>
2021-01-27 23:57:24 +0100

    code from andrzej's branch and fixed eslint errors

--- db/db_services.js
+++ db/db_services.js
@@ -1,4 +1,4 @@
-const {pool} = require('./config');
+const { pool } = require('./config');
 
 function disconnect() {
   pool.end();
@@ -9,7 +9,7 @@ async function get_user_id(username, password) {
   let args = [username, password];
   try {
     let res = await pool.query(query, args);
-    if(res.rows[0]){
+    if (res.rows[0]) {
       return res.rows[0].id;
     } else {
       return false;
@@ -101,7 +101,7 @@ async function add_product(price, name, size, colour, amount, status, descriptio
               VALUES ($1 ::numeric::money, $2, $3, $4, $5, $6, $7, $8)
               RETURNING id;`;
   let args = [price, name, size, colour, amount, status, description, category];
-  try{
+  try {
     let res = await pool.query(query, args);
     return res.rows[0].id;
   } catch (err) {
@@ -110,16 +110,6 @@ async function add_product(price, name, size, colour, amount, status, descriptio
   }
 }
 
-async function get_all_products(){
-  let query = 'SELECT * FROM product;'
-  try{
-    let res = await pool.query(query);
-    return res.rows;
-  } catch (err) {
-    console.error('db get_all_products error');
-    console.error(err);
-  }
-}
 
 async function set_product_status(prodid, status) {
   let query = `UPDATE product SET status = $1 WHERE id = $2;`;
@@ -145,23 +135,215 @@ async function set_product_amount(prodid, amount) {
   }
 }
 
-// eslint-disable-next-line no-unused-vars
 async function add_purchase_status(description) {
   let query = `INSERT INTO purchase_status (description) VALUES ($1);`;
   try {
     await pool.query(query, [description]);
     return true;
   } catch (err) {
-    console.error('db add_colour error');
+    console.error('db add_purchase_status error');
     console.error(err);
   }
 }
 
-//pool = connect();
-//add_purchase_status(pool, 'test2').then(res => {disconnect(pool)});
+async function add_picture(pool, prodid, filepath) {
+  let query = `INSERT INTO picture (product, filepath)
+              VALUES ($1, $2);`;
+  let args = [prodid, filepath];
+  try {
+    await pool.query(query, args);
+    return true;
+  } catch (err) {
+    console.error('db add_picture error');
+    console.error(err);
+  }
+}
 
-//TODO getters
-//TODO purchase management
+async function add_purchase(pool, userid, status) {
+  let query = `INSERT INTO purchase (userid, status)
+              VALUES ($1, $2);`;
+  let args = [userid, status];
+  try {
+    await pool.query(query, args);
+    return true;
+  } catch (err) {
+    console.error('db add_purchase error');
+    console.error(err);
+  }
+}
+
+async function add_sold_product(pool, purchaseid, prodid, amount) {
+  let query = `INSERT INTO sold_product (purchase_id, product_id, amount)
+              VALUES ($1, $2, $3);`;
+  let args = [purchaseid, prodid, amount];
+  try {
+    await pool.query(query, args);
+    return true;
+  } catch (err) {
+    console.error('db add_sold_product error');
+    console.error(err);
+  }
+}
+
+async function set_purchase_status(pool, purchaseid, status) {
+  let query = `UPDATE purchase SET status = $1 WHERE id = $2;`;
+  let args = [status, purchaseid];
+  try {
+    await pool.query(query, args);
+    return true;
+  } catch (err) {
+    console.error('db set_purchase_status error');
+    console.error(err);
+  }
+}
+
+async function get_category(pool, id) {
+  var query = `SELECT * FROM category;`;
+  var args = [];
+  if (id) {
+    query = 'SELECT * FROM category WHERE id = $1;';
+    args = [id];
+  }
+  try {
+    let res = await pool.query(query, args);
+    return res.rows;
+  } catch (err) {
+    console.error('db get_category error');
+    console.error(err);
+  }
+}
+
+async function get_colour(pool, id) {
+  var query = `SELECT * FROM colour;`;
+  var args = [];
+
+  if (id) {
+    query = `SELECT * FROM colour WHERE id = $1;`;
+    args = [id];
+  }
+
+  try {
+    let res = await pool.query(query, args);
+    return res.rows;
+  } catch (err) {
+    console.error('db get_colour error');
+    console.error(err);
+  }
+}
+
+async function get_picture(pool, prodid, id) {
+  var query = `SELECT * FROM picture WHERE product = $1;`;
+  var args = [prodid];
+
+  if (id) {
+    query = `SELECT * FROM picture WHERE id = $1;`;
+    args = [id];
+  }
+
+  try {
+    let res = await pool.query(query, args);
+    return res.rows;
+  } catch (err) {
+    console.error('db get_picture error');
+    console.error(err);
+  }
+}
+
+async function get_purchase(pool, userid, id) {
+  var query = "";
+  var args = [];
+  if (id) {
+    query = `SELECT * FROM purchase WHERE id = $1;`;
+    args = [id];
+  } else {
+    if (userid) {
+      query = `SELECT * FROM purchase WHERE userid = $1;`;
+      args = [userid];
+    } else {
+      query = `SELECT * FROM purchase;`;
+      args = [];
+    }
+  }
+  try {
+    let res = await pool.query(query, args);
+    return res.rows;
+  } catch (err) {
+    console.error('db get_purchase error');
+    console.error(err);
+  }
+}
+
+async function get_purchase_status(pool, id) {
+  var query = `SELECT * FROM purchase_status;`;
+  var args = [];
+
+  if (id) {
+    query = `SELECT * FROM purchase_status WHERE id = $1;`;
+    args = [id];
+  }
+
+  try {
+    let res = await pool.query(query, args);
+    return res.rows;
+  } catch (err) {
+    console.error('db get_purchase_status error');
+    console.error(err);
+  }
+}
+
+async function get_size(pool, id) {
+  var query = `SELECT * FROM size;`;
+  var args = [];
+
+  if (id) {
+    query = `SELECT * FROM size WHERE id = $1;`;
+    args = [id];
+  }
+
+  try {
+    let res = await pool.query(query, args);
+    return res.rows;
+  } catch (err) {
+    console.error('db get_size error');
+    console.error(err);
+  }
+}
+
+async function get_sold_product(pool, purchaseid) {
+  var query = `SELECT * FROM sold_product;`;
+  var args = [];
+
+  if (purchaseid) {
+    query = `SELECT * FROM sold_product WHERE purchase_id = $1;`;
+    args = [purchaseid];
+  }
+
+  try {
+    let res = await pool.query(query, args);
+    return res.rows;
+  } catch (err) {
+    console.error('db get_sold_product error');
+    console.error(err);
+  }
+}
+
+async function get_product(pool, id) {
+  var query = `SELECT * FROM product;`;
+  var args = [];
+
+  if (id) {
+    query = `SELECT * FROM product WHERE id = $1;`;
+    args = [id];
+  }
+
+  try {
+    let res = await pool.query(query, args);
+    return res.rows;
+  } catch (err) {
+    console.error('db get_product error');
+    console.error(err);
+  }
+}
 
 module.exports = {
   disconnect,
@@ -175,5 +357,17 @@ module.exports = {
   add_product,
   set_product_status,
   set_product_amount,
-  get_all_products
+  add_purchase_status,
+  add_picture,
+  add_purchase,
+  add_sold_product,
+  set_purchase_status,
+  get_category,
+  get_colour,
+  get_picture,
+  get_purchase,
+  get_purchase_status,
+  get_size,
+  get_sold_product,
+  get_product
 }
