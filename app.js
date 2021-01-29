a4ab4a728b95345f8c16d4820af6e82fda08225c
Maksymilian Czudziak <maks.czudziak@outlook.com>
2021-01-29 20:03:49 +0100

    first version of login, based on registration done

--- app.js
+++ app.js
@@ -38,6 +38,10 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
 router.get('/cart', (req, res) => {
     res.render('cart');
 })
