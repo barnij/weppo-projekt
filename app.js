ffe81f1ee79cf68a244e7b5ef4e39ade617872f9
Maksymilian Czudziak <maks.czudziak@outlook.com>
2021-01-29 20:17:23 +0100

    fixes of post and duplicate

--- app.js
+++ app.js
@@ -38,9 +38,6 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
-router.get('/login', (req, res) => {
-    res.render('login');
-});
 
 router.get('/cart', (req, res) => {
     res.render('cart');
